defcdaab79afd8e326b117ce6e203f96347b21e6
jafar_hamza <jafar_hamza@G-02-11>
2022-09-15 09:17:50 -0400

    Add project files.

---
+++ cclearncp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cclearncp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double XD = 5;
+
+            int XDD = Convert.ToInt32(XD);
+
+            Console.WriteLine(XDD);
+            
+        }
+    }
+}
